db255f85cab072fbe864a90a7070526de70a418a
tienhung164 <hungvt164@gmail.com>
2022-03-31 22:36:22 +0700

    done sign and vetify

--- app.js
+++ app.js
@@ -2,15 +2,54 @@ const express = require('express')
 const app = express()
 const PORT= process.env.PORT || 3000
 const jwt = require('jsonwebtoken')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
-app.use(express.json())
+
+var data=[{
+     id :'1',
+     name :'dog and cat'
+},{
+    id :'2',
+    name :'java and python'
+}]
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/',(req,res)=>{
      res.status(200).json({
          mess : "home"
      })
 })
+
+app.post('/login',(req,res)=>{
+    //authentical 
+
+    //authenrizon
+    let user =req.body.user
+    //console.log(user)
+    let acessToken= jwt.sign(user,process.env.ACCESS_KEY_JWT,{expiresIn:'1h'})
+    res.json({acessToken}) 
+
+})
+
+app.get('/list',middleware,(req,res)=>{
+
+    res.json({data})
+
+})
+
+function middleware(req,res,next){
+
+let token = req.headers['authorization'].split(' ')[1]
+    if(!token) res.status(401)
+    jwt.verify(token,process.env.ACCESS_KEY_JWT,(err,data)=>{
+        if(err) res.status(403)
+        if(data) next() 
+    })
+
+}
 
 
 app.listen(PORT,()=>{
